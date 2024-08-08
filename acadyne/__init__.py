d93acb153145c6f2cdb5af9d0e586026f5fc3317
randommusicd <randommusicd@gmail.com>
2024-08-07 19:37:00 -0600

    cambios de importacion  en el init principal

--- acadyne/__init__.py
+++ acadyne/__init__.py
@@ -1,4 +1,6 @@
-from .core.set_tensor import SetTensor
-from .core.tensor_bionico import BionicTensor
+from .tensores.set_tensor import SetTensor
+from .tensores.tensor_bionico import BionicTensor
+from .tensores.dynamic_tensor import DynamicTensor
 
-__all__ = ["SetTensor", "BionicTensor"]
+
+__all__ = ["SetTensor", "BionicTensor","DynamicTensor"]
